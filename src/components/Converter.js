956410a9bc790e42af13e317fcc78617e6df943e
Melissa-Erion <melissaerion26@gmail.com>
2023-12-14 12:07:19 -0700

    Add chart.js for historical currency rates

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Currencyinfo from "./Currencyinfo";
+import Chart from 'chart.js/auto';
+
 
 
 const Converter = () => {
@@ -8,6 +10,7 @@ const Converter = () => {
   const [targetCurrency, setTargetCurrency] = useState("EUR");
   const [amount, setAmount] = useState(1);
   const [currencies, setCurrencies] = useState({});
+  const chartRef = useRef(null);
 
   const handleSwapCurrencies = () => {
     setBaseCurrency(targetCurrency);
@@ -43,6 +46,109 @@ const Converter = () => {
 
     fetchCurrencies();
   }, []);
+
+  useEffect(() => {
+    const fetchHistoricalRates = async () => {
+      try {
+        const endDate = new Date().toISOString().split('T')[0];
+        const startDate = new Date((new Date()).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+        const response = await fetch(`https://api.frankfurter.app/${startDate}..${endDate}?from=${baseCurrency}&to=${targetCurrency}`);
+        const data = await response.json();
+
+        if (!data.error) {
+          const chartLabels = Object.keys(data.rates);
+          const chartData = Object.values(data.rates).map(rate => rate[targetCurrency]);
+          const chartLabel = `${baseCurrency}/${targetCurrency}`;
+
+          buildChart(chartLabels, chartData, chartLabel);
+        } else {
+          console.error("Error fetching historical rates:", data.error);
+        }
+      } catch (error) {
+        console.error("Error fetching historical rates:", error);
+      }
+    };
+
+    fetchHistoricalRates();
+  }, [baseCurrency, targetCurrency]);
+
+  const buildChart = (labels, data, label) => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+      if (ctx.chart) {
+        ctx.chart.destroy(); // Destroy the existing chart instance
+      }
+      ctx.chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels,
+          datasets: [{
+            label,
+            data,
+            fill: false,
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+          }],
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              labels: {
+                color: 'white', // Set the legend label color
+              },
+            },
+          },
+        },
+      });
+    }
+  };
+
+  useEffect(() => {
+    const fetchExchangeRate = async () => {
+      try {
+        const response = await fetch(
+          `https://api.frankfurter.app/latest?from=${baseCurrency}`
+        );
+        const data = await response.json();
+        setExchangeRate(data.rates);
+        buildChart(
+          Object.keys(data.rates),
+          Object.values(data.rates).map((rate) => rate[targetCurrency]),
+          `${baseCurrency}/${targetCurrency}`
+        );
+      } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+      }
+    };
+
+    fetchExchangeRate();
+  }, [baseCurrency, targetCurrency]);
+
+  useEffect(() => {
+    const fetchHistoricalRates = async () => {
+      try {
+        const endDate = new Date().toISOString().split('T')[0];
+        const startDate = new Date((new Date()).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+        const response = await fetch(`https://api.frankfurter.app/${startDate}..${endDate}?from=${baseCurrency}&to=${targetCurrency}`);
+        const data = await response.json();
+  
+        if (!data.error) {
+          const chartLabels = Object.keys(data.rates);
+          const chartData = Object.values(data.rates).map(rate => rate[targetCurrency] * amount); // Multiply by the amount
+          const chartLabel = `${baseCurrency}/${targetCurrency}`;
+  
+          buildChart(chartLabels, chartData, chartLabel);
+        } else {
+          console.error("Error fetching historical rates:", data.error);
+        }
+      } catch (error) {
+        console.error("Error fetching historical rates:", error);
+      }
+    };
+  
+    fetchHistoricalRates();
+  }, [baseCurrency, targetCurrency, amount]);
 
   const handleBaseCurrencyChange = (event) => {
     setBaseCurrency(event.target.value);
@@ -86,13 +192,13 @@ const Converter = () => {
         </select>
       </label>
       <p className="result">
-        {amount} {baseCurrency} is equal to{" "}
+        {amount} {baseCurrency} is equal to{' '}
         {exchangeRate
           ? (amount * exchangeRate[targetCurrency]).toFixed(2)
-          : "Loading..."}{" "}
+          : 'Loading...'}{' '}
         {targetCurrency}
       </p>
-
+      <canvas ref={chartRef} /> {/* Chart canvas */}
       <Currencyinfo baseCurrency={baseCurrency} amount={amount} />
     </div>
   );
